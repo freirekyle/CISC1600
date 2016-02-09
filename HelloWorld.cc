933f2ed6d6da958d340f5f28355df35d5446c97f
Kyle Freire <kylefreire@storm.cis.fordham.edu>
2016-02-08 20:54:57 -0500

    Homework1

---
+++ HelloWorld.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+		
+
+	cout<<"Hi, my name is aaa. I am bob years old, and my favorite thing is ccc"<< endl; 
+
+
+	return 0;
+}
+
